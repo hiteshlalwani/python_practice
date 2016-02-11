2306569338848acbf28a2ae241421f0bd064e947
Hitesh Lalwani <hitesh.lalwani007@gmail.com>
2016-02-12 01:51:10 +0530

    python practice exercises

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Hitesh Kumar Lalwani'
+my_age = 26
+my_height = 68 # inches
+my_weight = 147.4 # pounds (67 kg)
+my_eyes = 'Brown'
+my_teeth = 'off white'
+my_hair = 'black'
+
+print("Let's talk about %s" % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." %(my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee." % my_teeth)
+
+# this line is tricky, try to get it exactly right
+print("If I add %r, %d and %d I get %d" \
+(my_age, my_height, my_weight, my_age+my_height+my_weight))
+print("Phat gya")
